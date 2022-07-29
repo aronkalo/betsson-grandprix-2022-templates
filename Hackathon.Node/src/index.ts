6afd6799ba44dd3fe43f09974e2780bb27224e3c
Aron Zoltan Kalo <zoka01@betssongroup.com>
2022-07-29 12:51:23 +0200

    feature: node project template

--- Hackathon.Node/src/index.ts
+++ Hackathon.Node/src/index.ts
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log('hey there!')
+import map from './map.json';
+import { Position } from './position';
+import { Map } from './map';
+import fs from 'fs';
+
+console.log(`Input: ${JSON.stringify(map)}`);
+
+try {
+    const mapObject: Map = map;
+    const movement = calculateMovement(mapObject);
+    
+    const resultString = JSON.stringify(movement);
+    console.log(`Output: ${resultString}`);
+    fs.writeFileSync('./result.json', resultString, 'utf-8');
+}
+catch(error) {
+    console.error(error);
+}
+
+
+function calculateMovement(map: Map) : Position[] {
+    // TODO implement your changes here
+    return [ { x: 0, y: 0 } ];
+}
